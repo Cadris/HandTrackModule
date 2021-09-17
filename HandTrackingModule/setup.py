650b86833c7266e150e4554374a88000ed2d7f51
Cadris <cadrissilver@gmail.com>
2021-09-18 00:27:55 +0530

    HandTrackModule ready to publish to pypl

---
+++ HandTrackingModule/setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+classifiers = [
+	'Development Status :: 5 - Production/Stable',
+	'Intended Audience :: Education',
+	'Operating System :: Microsoft :: Windows :: Windows 10',
+	'License :: OSI Approved :: MIT License',
+	'Programming Language :: Python :: 3'
+]
+
+setup(
+	name='HandTrackingModule',
+	version='1.0.0',
+	description='Track the Hands from Web Cam (Default)',
+	long_description=open('README.txt').read() + '\n\n' + open('CHANGELOG.txt').read(),
+	url='https://github.com/Cadris/HandTrackModule',
+	author='Sayan Dasgupta',
+	author_email='cadrissilver@gmail.com',
+	license='MIT',
+	classifiers=classifiers,
+	keywords='hand handtracker handtrackermodule',
+	packages=find_packages(),
+	install_requires=['opencv-python', 'mediapipe']
+)
